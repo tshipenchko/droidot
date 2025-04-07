f654508df80bc2706a1f99d0009f627cd43cfcaa
tshipenchko <dev@zxc.sx>
2025-04-08 00:38:34 +0500

    Install utils

--- nix-on-droid.nix
+++ nix-on-droid.nix
@@ -25,12 +25,54 @@
     gnumake
     wget
     curl
+    axel
     lynx
     gawk
+    tmux
     comma
     nix-index
     openssh
     nixpkgs-fmt
+    iperf
+    micro
+    calc
+    htop
+    neofetch
+    fastfetch
+    ouch
+    nmap
+    zip
+    unzip
+    unrar-wrapper
+    nmap
+    iproute2
+    ethtool
+    nettools
+    inetutils
+    python3Packages.speedtest-cli
+    python3Packages.yt-dlp
+    file
+    jq
+    ffmpeg
+    dig
+    binutils
+    poppler_utils
+    calc
+    postgresql
+    net-snmp
+
+    python3
+    pypy3
+    black
+    isort
+    ruff
+    gcc
+    gnumake
+    cmake
+    nodejs
+    rustup
+    go
+    gofumpt
   ];
 
   environment.etcBackupExtension = ".bak";
